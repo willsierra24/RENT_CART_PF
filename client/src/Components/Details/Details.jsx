0bc5369030087e4fbd6d7b2dd1ff434f03e0d476
willsierra24 <willjusi@gmail.com>
2023-02-03 09:25:26 -0500

    Landing and details

---
+++ client/src/Components/Details/Details.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+import "./Details.css"
+
+export default function Details() {
+const {state} = useLocation();
+  
+  return (
+    <div className='container1'>
+      <h1>{state.name}</h1>
+      <img src={state.image} alt="car" height="120px"/>
+      <h1>{state.price} </h1>
+    </div>
+  )
+}
