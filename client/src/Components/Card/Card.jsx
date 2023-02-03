8e48b969c1aaa7bd795afc1b896c9f670a82a589
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-02-03 10:27:40 -0500

    cambios

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import './card.css';
-import {Link} from 'react-router-dom';
 
-const Card = ({car}) => {
+const Card = () => {
   return (
-    <Link to={`/detail/${car.id_license_plare}`} state={car} className = 'link'>
-		<div className='card'>
-      <div className='texts'>		      
-        <div className='text'>{car.name}</div>
-        <div className='text2'>AUT 5P A/C  </div>	
-        <div className='text3'>US$ {car.price}</div>	
-      </div>
-      <img className='img' src={car.image} alt={"No"} />
-		</div>
-      </Link>	
+    <div>Card</div>
   )
 }
 
