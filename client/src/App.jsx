a8f437299f36ab4496a979a9a24afdc53ed54578
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-02-03 11:03:06 -0500

    actualizado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import RouteApp from "./routes/RouteApp";
+import Footer from "./Footer/Footer";
+import NavBar from  './NavBar/NavBar'
 
 function App() {
   return (
     <>
+      <NavBar/>
       <RouteApp />
+      <Footer />
     </>
   );
 }
