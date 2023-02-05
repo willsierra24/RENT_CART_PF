d4fd30963835d154a90fad3c03b2a1b54e65e6cb
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-02-05 13:05:51 -0500

    update serach input

--- client/src/Components/Search/Search.jsx
+++ client/src/Components/Search/Search.jsx
@@ -3,23 +3,23 @@ import styled from "styled-components";
 import axios from "axios";
 
 function Search() {
-  const [cars, setCars] = useState([]);
+  // const [cars, setCars] = useState([]);
   const [search, setSearch] = useState("");
 
-  const API_URL = `http://localhost:3001/cars?q=${location}`;
-  const infoApi = async () => {
-    try {
-      const data = await axios.get(API_URL);
-      console.log(data);
-      setCars(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const API_URL = "http://localhost:3001/cars";
+  // const infoApi = async () => {
+  //   try {
+  //     const data = await axios.get(API_URL);
+  //     console.log(data);
+  //     setCars(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  React.useEffect(() => {
-    infoApi();
-  }, []);
+  // React.useEffect(() => {
+  //   infoApi();
+  // }, []);
 
   function handleLocation(e) {
     setSearch(e.target.value);
@@ -39,12 +39,8 @@ function Search() {
             value={search}
             placeholder="¿Donde necesitas tu auto?"
             onChange={(e) => handleLocation(e)}
-          ></InputStyled>
-          {/* <input type="date"></input>
-          <input type="time"></input>
-          <input type="text" placeholder="¿Dónde termina tu viaje?"></input>
-          <input type="date"></input>
-          <input type="time"></input> */}
+          />
+
           <button>Buscar</button>
         </form>
       </ContainerStyled>
@@ -64,7 +60,7 @@ export const InputStyled = styled.input`
   border: 2px solid #fb8500;
   border-radius: 8px;
   margin-right: 3px;
-  padding: 5px;
+  padding: 8px;
   outline: none;
   color: black;
 `;
